896011ea772d4f98ea5ebbac2085159b6714806b
chisanucha-suwan <chisanucha.s@ku.th>
2021-07-10 14:04:49 +0700

    Initial commit

---
+++ views/layout/header.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta name="description" content="Ashion Template">
+<meta name="keywords" content="Ashion, unica, creative, html">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>OTOP</title>
+<link rel="shortcut icon" type="image/x-icon" href="./img/icon/market.png" />
+
+<?php include_once("css.php") ?>
