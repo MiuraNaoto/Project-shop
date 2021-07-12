c45e3fcc9185938242748e43be079dde42db2677
AlwaysByMe <ddeam43@gmail.com>
2021-07-12 23:31:08 +0700

    profile

--- views/Customer/profile-shop/profile-shop.php
+++ views/Customer/profile-shop/profile-shop.php
@@ -3,6 +3,7 @@
 
 <head>
     <?php include_once("../layout/header.php") ?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
@@ -43,117 +44,116 @@
                 </div>
                 <div class="col-md-7">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="margin-top: 45px;">
                             <span><i class="fas fa-boxes" style="margin-right: 10px;"></i>สินค้าทั้งหมด 108 รายการ</span>
+                            <br>
+                            <br>
+                            <span><i class="fas fa-user-clock" style="margin-right: 10px;"></i>เข้าร่วมเมื่อ: 1 เดือน ที่ผ่านมา</span>
                         </div>
-                        <div class="col-md-6">
-
+                        <div class="col-md-6" style="margin-top: 45px;">
+                            <span><i class="fas fa-star-half-alt" style="margin-right: 10px;"></i>คะแนน: 4.5(จากคะแนนทั้งหมด 400)</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- <div class="row">
-                <div class="col-lg-12 text-center">
+
+            <div class="row">
+                <div class="col-lg-12 text-center" style="margin-top: 40px;">
                     <div class="related__title">
-                        <h5>RELATED PRODUCTS</h5>
+                        <h5>รายการสินค้า</h5>
                         <hr>
                         <br>
                     </div>
                 </div>
+            </div>
+            <div class="card text-center">
+                <div class="card-header">
 
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-1.jpg">
-                            <div class="label new">New</div>
-                            <ul class="product__hover">
-                                <li><a href="../../../img/product/related/rp-1.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Buttons tweed blazer</a></h6>
-                            <div class="rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">หน้าแรก</a>
+                        <a class="nav-item nav-link" id="nav-allitem-tab" data-toggle="tab" href="#nav-allitem" role="tab" aria-controls="nav-allitem" aria-selected="false">สินค้าทั้งหมด</a>
+                        <a class="nav-item nav-link" id="nav-shirt-tab" data-toggle="tab" href="#nav-shirt" role="tab" aria-controls="nav-shirt" aria-selected="false">เสื้อ</a>
+                        <a class="nav-item nav-link" id="nav-shirt-tab" data-toggle="tab" href="#nav-shirt" role="tab" aria-controls="nav-shirt" aria-selected="false">กางเกง</a>
+                        <a class="nav-item nav-link" id="nav-shirt-tab" data-toggle="tab" href="#nav-shirt" role="tab" aria-controls="nav-shirt" aria-selected="false">น้ำหอม</a>
+                        <a class="nav-item nav-link" id="nav-shirt-tab" data-toggle="tab" href="#nav-shirt" role="tab" aria-controls="nav-shirt" aria-selected="false">ต้นไม้</a>
+                        <a class="nav-item nav-link" id="nav-shirt-tab" data-toggle="tab" href="#nav-shirt" role="tab" aria-controls="nav-shirt" aria-selected="false">อุปกรณ์</a>
+                    </div>
+
+                    <div class="card-body tab-content" id="nav-tabContent">
+                        <div class="col-lg-3 col-md-4 col-sm-6 tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-2.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="../../../img/product/related/rp-2.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">Flowy striped skirt</a></h6>
+                                    <div class="rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <div class="product__price">$ 49.0</div>
+                                </div>
                             </div>
-                            <div class="product__price">$ 59.0</div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 tab-pane fade" id="nav-allitem" role="tabpanel" aria-labelledby="nav-allitem-tab">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-3.jpg">
+                                    <div class="label stockout">out of stock</div>
+                                    <ul class="product__hover">
+                                        <li><a href="../../../img/product/related/rp-3.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">Cotton T-Shirt</a></h6>
+                                    <div class="rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <div class="product__price">$ 59.0</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 tab-pane fade" id="nav-shirt" role="tabpanel" aria-labelledby="nav-shirt-tab">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-4.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="../../../img/product/related/rp-4.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">Slim striped pocket shirt</a></h6>
+                                    <div class="rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <div class="product__price">$ 59.0</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-2.jpg">
-                            <ul class="product__hover">
-                                <li><a href="../../../img/product/related/rp-2.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Flowy striped skirt</a></h6>
-                            <div class="rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                            </div>
-                            <div class="product__price">$ 49.0</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-3.jpg">
-                            <div class="label stockout">out of stock</div>
-                            <ul class="product__hover">
-                                <li><a href="../../../img/product/related/rp-3.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Cotton T-Shirt</a></h6>
-                            <div class="rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                            </div>
-                            <div class="product__price">$ 59.0</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" data-setbg="../../../img/product/related/rp-4.jpg">
-                            <ul class="product__hover">
-                                <li><a href="../../../img/product/related/rp-4.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
-                                <li><a href="#"><span class="icon_heart_alt"></span></a></li>
-                                <li><a href="#"><span class="icon_bag_alt"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">Slim striped pocket shirt</a></h6>
-                            <div class="rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                            </div>
-                            <div class="product__price">$ 59.0</div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
+            </div>
         </div>
+
     </section>
+
     <!-- Product Details Section End -->
 
     <!-- Js Plugins -->
