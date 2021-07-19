8981745f67b59fb5345b8bce3839a6cf64e20a1b
chisanucha-suwan <chisanucha.s@ku.th>
2021-07-20 00:07:21 +0700

    Update user-profileModal.php

--- views/Customer/user-profile/user-profileModal.php
+++ views/Customer/user-profile/user-profileModal.php
@@ -204,7 +204,7 @@
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">เพิ่มที่อยู่จัดส่ง</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">แก้ไขที่อยู่จัดส่ง</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
