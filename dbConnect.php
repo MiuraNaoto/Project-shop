15e5862393dd1b6080758d3042b552a5e35e7d8c
sLanc3lot <weerin.w@ku.th>
2021-12-04 20:52:17 +0700

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -11,7 +11,7 @@ function connectDB()
     // $password = "Cpekps@u2t";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=U2T;charset=utf8", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=otop_shop;charset=utf8", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //$conn->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
